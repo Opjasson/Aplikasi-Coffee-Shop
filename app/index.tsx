c912ba65ede4d4043d7f87fbbc6a0b5651f37817
Opjasson <azmitheusos333@gmail.com>
2025-02-22 16:08:55 +0700

    setting stack navigator

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+    NavigationContainer,
+    NavigationIndependentTree,
+} from "@react-navigation/native";
 import { Text, View } from "react-native";
 import { SplashScreen } from "./pages";
-import 'react-native-gesture-handler'
+import "react-native-gesture-handler";
+import Router from "./router";
 
 export default function Index() {
-  return (
-    <NavigationContainer>
-      <SplashScreen />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationIndependentTree>
+            <NavigationContainer>
+                <Router />
+            </NavigationContainer>
+        </NavigationIndependentTree>
+    );
 }
--- app/router/index.tsx
+++ app/router/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Home, SplashScreen } from "../pages";
 
 const Stack = createStackNavigator();
 const Router = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator screenOptions={{ headerShown : false }}>
+            <Stack.Screen name="SplashScreen" component={SplashScreen} />
+            <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
     );
 };
