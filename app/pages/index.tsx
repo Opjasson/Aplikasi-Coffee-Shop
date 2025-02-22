aeb5faa695e44a4f7fd2190cbe9c4e5e8d59599a
Opjasson <azmitheusos333@gmail.com>
2025-02-22 19:41:31 +0700

    add tabNavigator

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,13 @@
 import SplashScreen from "./splashscreens";
 import Home from "./home"
+import Favorite from "./favorite";
+import Cart from "./cart";
+import Profile from "./profile";
 
 export {
     SplashScreen,
-    Home
+    Home,
+    Favorite,
+    Cart,
+    Profile
 }
--- app/router/index.tsx
+++ app/router/index.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home, SplashScreen } from "../pages";
+import { Cart, Favorite, Home, Profile, SplashScreen } from "../pages";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+
+const MainApp = () => {
+  return (
+      <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Favorite" component={Favorite} />
+          <Tab.Screen name="Cart" component={Cart} />
+          <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+  );
+}
+
+
+
 const Router = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown : false }}>
             <Stack.Screen name="SplashScreen" component={SplashScreen} />
             <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="MainApp" component={MainApp} />
         </Stack.Navigator>
     );
 };
