ea62ebe5e438e98fc1d7f9b05714943541fc2eb3
Opjasson <azmitheusos333@gmail.com>
2025-02-22 23:18:03 +0700

    button menu

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,5 +1,5 @@
-import { photo } from "../../inventory/images";
-import { Location, Notification, Search, Filter } from "../../inventory/icons";
+import { coffe, photo } from "../../inventory/images";
+import { Location, Notification, Search, Filter, Cup } from "../../inventory/icons";
 import React from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
@@ -35,15 +35,57 @@ const index = () => {
                 </TouchableOpacity>
             </View>
             {/* End top menu */}
-
-            <View style={{ flexDirection: "row", justifyContent: 'space-between', width : '75%', backgroundColor : '#1111', marginHorizontal : 'auto', borderRadius : 30, paddingHorizontal : 20, marginTop : 20  }}>
-                <View style={{ flexDirection: "row", alignItems : 'center', gap : 2 }}>
+            <View style={{ marginHorizontal: 30, marginTop: 15 }}>
+                <Text style={{ fontWeight: "500", fontSize: 14 }}>
+                    Good Morning, Jasson Ghazy
+                </Text>
+            </View>
+            {/* Search tab */}
+            <View
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: "75%",
+                    backgroundColor: "#1111",
+                    marginHorizontal: "auto",
+                    borderRadius: 30,
+                    paddingHorizontal: 20,
+                    marginTop: 20,
+                }}>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 2,
+                    }}>
                     <Image source={Search} />
                     <TextInput placeholder="Search Coffe ..." />
                 </View>
-                <View style={{ justifyContent : 'center' }}>
+                <View style={{ justifyContent: "center" }}>
                     <Image source={Filter} />
                 </View>
+            </View>
+            {/* End Search */}
+
+            <View>
+                <View>
+                    <Text>Category</Text>
+                </View>
+                <TouchableOpacity
+                    style={{
+                        justifyContent : 'center',
+                        flexDirection: "row",
+                        backgroundColor: "#2f823a",
+                        alignItems : 'center',
+                        paddingHorizontal : 15,
+                        paddingVertical : 15,
+                        borderRadius : 30,
+                        width : 140,
+                        gap : 5
+                    }}>
+                    <Image source={Cup} />
+                    <Text style={{ color : 'white' }}>Cappuchino</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
